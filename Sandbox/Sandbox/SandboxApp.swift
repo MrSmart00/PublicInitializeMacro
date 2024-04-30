591931ba574128f0aee58120c6c22f64275dbc2f
MrSmart00 <nexearth@gmail.com>
2024-04-30 17:52:25 +0900

    Add: create project foundation

---
+++ Sandbox/Sandbox/SandboxApp.swift
@@ -0,0 +1,18 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by 日野森寛也 on 2024/04/30.
+//
+
+import SwiftUI
+import App
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
